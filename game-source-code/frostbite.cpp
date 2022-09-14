1c63d9e01db35a2a8e8ad0488840546a7eecc83f
Koshti01 <ruthannwright01@gmail.com>
2022-09-14 16:23:41 +0200

    Basic Frostbite Class

--- game-source-code/frostbite.cpp
+++ game-source-code/frostbite.cpp
@@ -1,4 +1,50 @@
 //Screen implementation
-#include <SFML/Graphics.hpp>
-#include "frostbite.h"
+#include "Frostbite.h"
+Frostbite::~Frostbite()
+{
+}
+Frostbite::Frostbite(std::string imDirectory, const sf::Vector2f &Size)
+{
+    if(!pTexture.loadFromFile(imDirectory))
+    {
+        std::cerr<<"Error\n"; //display error if it does not load
+    }
+    pSprite.setTexture(pTexture);
+    pSprite.setScale(Size);
+}
+void Frostbite::drawPlayer(sf::RenderWindow &window)
+{
+    window.draw(pSprite);
+}
+//move player around
+void Frostbite::movePlayer(char direction, float moveSpeed)
+{
+    if(direction == 'u')
+    {
+        pSprite.move(0, -moveSpeed);
+    }
+    else if(direction == 'd')
+    {
+        pSprite.move(0, moveSpeed);
+    }
+    else if(direction == 'l')
+    {
+        pSprite.move(-moveSpeed, 0);
+    }
+    else if(direction == 'r')
+    {
+        pSprite.move(moveSpeed, 0);
+    }
+}
+//get and set players positon (should be center of player for now)
+const sf::Vector2f Frostbite::getPlayerPosition() const
+{
+    return pSprite.getPosition();
+}
+void Frostbite::setPlayerPostion(float x, float y)
+{
+    pSprite.setPosition(x,y);
+}
+
+
 
--- game-source-code/frostbite.h
+++ game-source-code/frostbite.h
@@ -1,11 +1,31 @@
-#ifndef FROSTBITE
-#define FROSTBITE
+#ifndef FROSTBITE_H
+#define FROSTBITE_H
 
-/** \file screen.h
-    \brief sets dimenstion of screen ....
-    NB:to document global objects (functions, typedefs, enum, macros, etc), you must document the file in which they are defined. That is,
-    you must provide this file comment block.
- */
+#include <iostream>
+#include <SFML\Graphics.hpp>
 
-#include <SFML/Graphics.hpp>
-#endif
+class Frostbite
+{
+    public:
+        Frostbite(){
+        //default
+        }
+        Frostbite(std::string imDirectory, const sf::Vector2f &Size);
+        void drawPlayer(sf::RenderWindow &window);
+
+        //move player around
+        void movePlayer(char direction, float moveSpeed);
+        //get and set players positon (should be center of player for now)
+        const sf::Vector2f getPlayerPosition() const ;
+        void setPlayerPostion(float x, float y);
+        virtual ~Frostbite();
+
+    protected:
+
+    private:
+        sf::Texture pTexture;
+        sf::Sprite pSprite;
+};
+
+#endif // FROSTBITE_H
+
