3f837642ba42cc188940fd598372b37e06ecf4f9
sendr7 <d.sender07@gmail.com>
2022-09-16 12:45:55 +0200

    Iceberg Class Setup

--- game-source-code/iceberg.cpp
+++ game-source-code/iceberg.cpp
@@ -1,4 +1,47 @@
 //Screen implementation
-#include <SFML/Graphics.hpp>
 #include "iceberg.h"
 
+Iceberg::~Iceberg()
+{
+}
+
+Iceberg::Iceberg(std::string imDirectory, const sf::Vector2f &Size)
+{
+    if(!iTexture.loadFromFile(imDirectory))
+    {
+        std::cerr<<"Error\n"; //display error if it does not load
+    }
+    iSprite.setTexture(iTexture);
+    iSprite.setScale(Size);
+    iSprite.setOrigin(iSprite.getScale().x/2,iSprite.getScale().y/2);
+}
+
+void Iceberg::drawIceberg(sf::RenderWindow &window)
+{
+    window.draw(iSprite);
+}
+
+void Iceberg::moveIceberg(char direction, float moveSpeed, const float gameHeight, const float gameWidth, sf::Sprite &overlapIceberg)
+{
+
+}
+
+const sf::Vector2f Iceberg::getIcebergPosition() const
+{
+
+}
+
+void setIcebergPostion(float x, float y)
+{
+
+}
+
+const float Iceberg::getIcebergWidth() const
+{
+
+}
+
+const float Iceberg::getIcebergHeight() const
+{
+
+}
--- game-source-code/iceberg.h
+++ game-source-code/iceberg.h
@@ -1,11 +1,31 @@
 #ifndef ICEBERG
 #define ICEBERG
 
-/** \file screen.h
-    \brief sets dimenstion of screen ....
-    NB:to document global objects (functions, typedefs, enum, macros, etc), you must document the file in which they are defined. That is,
-    you must provide this file comment block.
- */
+#include <iostream>
+#include <SFML\Graphics.hpp>
 
-#include <SFML/Graphics.hpp>
+class Iceberg
+{
+    public:
+        Iceberg(){
+        //default constructor
+        }
+        Iceberg(std::string imDirectory, const sf::Vector2f &Size);
+        //draws the iceberg on the window
+        void drawIceberg(sf::RenderWindow &window);
+        //moves the iceberg
+        void moveIceberg(char direction, float moveSpeed, const float gameHeight, const float gameWidth, sf::Sprite &overlapIceberg);
+        //getter and setter for the position of the iceberg
+        const sf::Vector2f getIcebergPosition() const;
+        void setIcebergPostion(float x, float y);
+        //get iceberg dimensions
+        const float getIcebergWidth() const;
+        const float getIcebergHeight() const;
+        virtual ~Iceberg();
+
+    private:
+        sf::Texture iTexture;
+        sf::Sprite iSprite;
+};
+
 #endif
