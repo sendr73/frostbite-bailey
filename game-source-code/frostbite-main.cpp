9b078bb0839c6e5f4c59ade524b320ca053a6dd1
Koshti01 <ruthannwright01@gmail.com>
2022-09-21 22:03:00 +0200

    Adjusting main.cpp to work with screen

--- game-source-code/frostbite-main.cpp
+++ game-source-code/frostbite-main.cpp
@@ -3,31 +3,46 @@
 ////////////////////////////////////////////////////////////
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-//#include <cmath>
-//#include <cstdlib>
-//#include <ctime>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include "screen.h"
+
+using namespace std;
 
 int main()
 {
-    const int gameWidth = 800; //Is this specified to be a particular value?
-    const int gameHeight = 600;
+    const auto gameWidth = 800.f; //Should change to 1000.f and 800.f (just too big for my laptop)
+    const auto gameHeight = 600.f;
+    const float frostbiteSpeed = 600.f;
+    const float icebergSpeed = 150.f;
 
-    sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight, 32), "SFML FrostBite", sf::Style::Titlebar | sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(gameWidth, gameHeight), "");
+    window.setVerticalSyncEnabled(true);
+
+    Screen screen(window);
+
+    sf::Clock clock;
+
     while(window.isOpen( ))
     {
-        sf::Event evnt;
+        sf::Event evnt; //not using reserved word event
+        bool pressed = false; //used to stop multiple jumps
         while(window.pollEvent(evnt))
         {
-            switch (evnt.type)
+            if (evnt.type == sf::Event::Closed)
             {
-            case sf::Event::Closed:
                 window.close();
-                break;
-            default:
-                break;
             }
+            screen.frostbiteJump(window, evnt, pressed);
         }
+        float deltaTime = clock.restart().asSeconds();
+        screen.moveFrostbite(window, frostbiteSpeed, deltaTime);
+        screen.moveIcerow(window, icebergSpeed, deltaTime); // (window,icebergSpeed,deltaTime);
+        screen.icebergCollision(window,icebergSpeed,deltaTime);
+        screen.refresh(window);
+        window.display();
     }
 
     return 0;
