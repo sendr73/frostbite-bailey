9a8f023c564c799616de49b54444abfc040a7c17
sendr7 <d.sender07@gmail.com>
2022-09-21 17:38:39 +0200

    Screen Class Setup

--- game-source-code/screen.cpp
+++ game-source-code/screen.cpp
@@ -1,4 +1,35 @@
 //Screen implementation
-#include <SFML/Graphics.hpp>
 #include "screen.h"
 
+using namespace std;
+
+Screen::Screen(sf::RenderWindow &window)
+{
+    setBackground(window);
+}
+
+void Screen::setBackground(const sf::RenderWindow &window)
+{
+    sf::RectangleShape sky(sf::Vector2f(window.getSize().x,0.2*window.getSize().y));
+    sky.setFillColor(sf::Color(78,119,195,255));
+    sky.setPosition(0.f,0.2*window.getSize().y);
+    sf::RectangleShape border(sf::Vector2f(window.getSize().x,0.01*window.getSize().y));
+    border.setFillColor(sf::Color(204,117,190,255));
+    border.setPosition(0.f,0.19*window.getSize().y);
+    sf::RectangleShape safezone(sf::Vector2f(window.getSize().x,0.19*window.getSize().y));
+    safezone.setFillColor(sf::Color(156,156,156,255));
+    if (!texture.create(window.getSize().x,0.4*window.getSize().y))
+    {
+        //issue returning error from void, maybe try{catch}?
+    }
+    texture.clear();
+    texture.draw(safezone);
+    texture.draw(border);
+    texture.draw(sky);
+    background.setTexture(texture.getTexture());
+}
+
+Screen::~Screen()
+{
+    //dtor
+}
--- game-source-code/screen.h
+++ game-source-code/screen.h
@@ -1,11 +1,47 @@
 #ifndef SCREEN
 #define SCREEN
 
-/** \file screen.h
-    \brief sets dimenstion of screen ....
-    NB:to document global objects (functions, typedefs, enum, macros, etc), you must document the file in which they are defined. That is,
-    you must provide this file comment block.
- */
+#include <iostream>
+#include <vector>
+#include <SFML\Graphics.hpp>
+#include "iceberg.h"
+#include "frostbite.h"
 
-#include <SFML/Graphics.hpp>
+using namespace std;
+
+class Screen
+{
+public:
+    Screen(){
+        //default
+    }
+    //parametized constructor
+    Screen(sf::RenderWindow &window);
+    //making the frostbite object jump on window, based on events
+    void frostbiteJump(const sf::RenderWindow &window, const sf::Event &event, bool &pressed);
+    //moving the frostbite object on window
+    void moveFrostbite(const sf::RenderWindow &window, const float &speed, const float &deltaTime);
+    //moving the rows of ice on screen
+    void moveIcerow(sf::RenderWindow &window, const float &speed, const float &deltaTime);
+    //collsion logic between frostbite and icebergs
+    void icebergCollision(sf::RenderWindow &window,const float &, const float &);
+    //refreshes the window
+    void refresh(sf::RenderWindow &window);
+    //destructor
+    virtual ~Screen();
+
+protected:
+    sf::RenderTexture texture; //texture object
+    sf::Sprite background; //sprite object for background
+    Frostbite frostbite=Frostbite("frostbite.png",sf::Vector2f(1.f,1.f)); //frostbite object
+    Iceberg iceberg=Iceberg("iceberg.png",sf::Vector2f(1.f,1.f)); //iceberg object
+    float row=4; //amount of rows (default of 4)
+    float column=4; //amount of columns (default of 3 + overlap)
+    vector<vector<Iceberg>> icerow; //2D vector of icebergs (could segregate into classes)
+    void setBackground(const sf::RenderWindow &window); //sets background on screen
+    void setFrostbite(const sf::RenderWindow &window); //sets frostbite properties on screen
+    void setIcebergRows(const sf::RenderWindow &window); //sets iceberg properties on screen
+    bool isOnIceberg(const Iceberg &iceberg); //helper that returns if frostbite is on an iceberg
+};
+
 #endif
