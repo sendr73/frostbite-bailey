d835c86bc86fef2907c36333fede7f8aabdad989
Koshti01 <ruthannwright01@gmail.com>
2022-10-03 16:16:09 +0200

    Updating Main file

--- game-source-code/frostbite-main.cpp
+++ game-source-code/frostbite-main.cpp
@@ -8,6 +8,7 @@
 #include <ctime>
 #include <iostream>
 #include "screen.h"
+#include "splashscreen.h"
 using namespace std;
 
 int main()
@@ -23,38 +24,9 @@ int main()
     //create screen object to run the game
     Screen screen(window);
     //setting up splash screen
+    splashScreen splash_screen(gameWidth, gameHeight);
     auto drawSplashScreen = true;
-    sf::Text header;
-    sf::Font font;
-    if (!font.loadFromFile("resources/sansation.ttf"))
-    {
-        std::cout<<"Error Cannot load Font";
-    }
-    header.setFont(font); //set font, color etc
-    header.setString("Frostbite Bailey");
-    header.setCharacterSize(60);
-    header.setFillColor(sf::Color::Blue);
-    header.setStyle(sf::Text::Bold|sf::Text::Underlined);
-    sf::FloatRect text_rectangle = header.getLocalBounds();
-    //set positon of header
-    header.setOrigin(text_rectangle.left + text_rectangle.width/2.0f,
-                   text_rectangle.top  + text_rectangle.height/2.0f);
-    header.setPosition(sf::Vector2f(gameWidth/2.0f,gameHeight/4.0f));
 
-    sf::Text paragraph;
-    paragraph.setString("Use the LEFT and RIGHT arrow keys to move Frostbite left and right \n"
-                        "and the UP and DOWN arrow keys to jump platforms, up and down. Try \n"
-                        "land on the moving icebergs. If Frostbite falls into the water, he \n"
-                        "will respawn in the middle of the safezone. \n \n"
-                        "Press the ENTER key to start.");
-    paragraph.setFont(font); //set font, color etc
-    paragraph.setCharacterSize(30);
-    paragraph.setFillColor(sf::Color::White);
-    text_rectangle = paragraph.getLocalBounds();
-    //set positon of paragraph
-    paragraph.setOrigin(text_rectangle.left + text_rectangle.width/2.0f,
-                   text_rectangle.top  + text_rectangle.height/2.0f);
-    paragraph.setPosition(sf::Vector2f(gameWidth/2.0f,gameHeight/2.0f));
 
     sf::Clock clock;
 
@@ -76,8 +48,7 @@ int main()
         }
         if(drawSplashScreen) //if the game has not yet started show instructions
         {
-            window.draw(header);
-            window.draw(paragraph);
+            splash_screen.draw(window);
             window.display();
             float deltaTime = clock.restart().asSeconds(); //Whithout this there might only be 1 iceberg per row
         }
