97903ecf9f576a0a98ac8e48864979c2e65899b6
sendr7 <d.sender07@gmail.com>
2022-10-04 10:21:20 +0200

    Score class implementation

---
+++ game-source-code/score.cpp
@@ -0,0 +1,53 @@
+#include "score.h"
+
+Score::Score()
+{
+    point = 10;
+    score = 0;
+    int level = 1;
+    int lives = 3;;
+}
+//getter for score
+const int Score::getScore() const
+{
+    return score;
+}
+//increases score
+void Score::increaseScore()
+{
+    score=score+point;
+}
+//increases score based on remaining temperature
+void Score::increaseScore(const int &temp)
+{
+    score=score+(temp*point);
+}
+//gets current level
+const int Score::getLevel() const
+{
+    return level;
+}
+//increases level
+void Score::increaseLevel()
+{
+    level++;
+}
+//gets current lives
+const int Score::getLives() const
+{
+    return lives;
+}
+//increases lives
+void Score::increaseLives()
+{
+    lives++;
+}
+//decreases lives
+void Score::decreaseLives()
+{
+    lives--;
+}
+Score::~Score()
+{
+    //dtor
+}
---
+++ game-source-code/score.h
@@ -0,0 +1,32 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+
+class Score
+{
+    public:
+        Score();
+        //getter and increment for score
+        const int getScore() const;
+        void increaseScore();
+        // case when round ends, add score proportional to temperature left
+        void increaseScore(const int &temp);
+        //getter and increment for level
+        const int getLevel() const;
+        void increaseLevel();
+        //getter and increment for lives
+        const int getLives() const;
+        void increaseLives();
+        void decreaseLives();
+        virtual ~Score();
+
+    protected:
+
+    private:
+        int point;
+        int score;
+        int level;
+        int lives;
+};
+
+#endif // SCORE_H
