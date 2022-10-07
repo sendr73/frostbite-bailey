07701c9ab349ae1afdbd14d04b1fb3ed603e97a1
Koshti01 <ruthannwright01@gmail.com>
2022-10-07 18:37:31 +0200

    Using base class with frosbite

--- game-source-code/frostbite.cpp
+++ game-source-code/frostbite.cpp
@@ -1,24 +1,15 @@
 //Screen implementation
 #include "Frostbite.h"
-Frostbite::~Frostbite()
+Frostbite::Frostbite(std::string imDirectory, const sf::Vector2f &Size): Element(imDirectory, Size)
 {
-}
-Frostbite::Frostbite(std::string imDirectory, const sf::Vector2f &Size)
-{
-    if(!fTexture.loadFromFile(imDirectory))
-    {
-        std::cerr<<"Error\n"; //display error if it does not load
-    }
-    fSprite.setTexture(fTexture);
-    fSprite.setScale(Size);
-    fSprite.setOrigin(getWidth()/2,getHeight()); //set orign to bottom centre
-    setPostion(getWidth()/2,getHeight()); //set Sprite to top left
+    setPosition(getWidth()/2,getHeight()); //set Sprite to top left
     jumped = false;
 }
-void Frostbite::draw(sf::RenderWindow &window)
+/*void Frostbite::draw(sf::RenderWindow &window)
 {
     window.draw(fSprite);
 }
+*/
 //move player around
 void Frostbite::move(char direction, const sf::RenderWindow &window, float moveSpeed)
 {
@@ -32,80 +23,85 @@ void Frostbite::move(char direction, const sf::RenderWindow &window, float moveS
     {
         direction = 'r';
     }
-    if(direction == 'l') //allows for presset direction
+    moveElement(direction,moveSpeed);
+
+    //screen colision on the left
+    if(getPosition().x<0 + getWidth()/2)
     {
-        fSprite.move(-moveSpeed, 0);
-        //screen colision on the left
-        if(getPosition().x<0 + getWidth()/2)
-        {//adjust the sides by width/2 when checking left and right
-            setPostion(getWidth()/2,getPosition().y);
-            //setPostion(gameWidth/2,0.375*gameHeight);//set Sprite to top middle
-            //return true; //indicate there was a clash
-        }
+        //adjust the sides by width/2 when checking left and right
+        setPosition(getWidth()/2,getPosition().y);
+        //setPostion(gameWidth/2,0.375*gameHeight);//set Sprite to top middle
+        //return true; //indicate there was a clash
     }
-    else if(direction == 'r')
+
+    //screen colision on the right
+    if(getPosition().x>gameWidth-getWidth()/2) //adjust the sides by width/2 when checking left and right
     {
-        fSprite.move(moveSpeed, 0);
-        //screen colision on the right
-        if(getPosition().x>gameWidth-getWidth()/2) //adjust the sides by width/2 when checking left and right
-         {
-             setPostion(gameWidth-getWidth()/2, getPosition().y);
-             //return true;
-         }
+        setPosition(gameWidth-getWidth()/2, getPosition().y);
+        //return true;
     }
+
     //return false;
 }
 //make player jump
 void Frostbite::jump(char direction, float moveSpeed, const float gameHeight, const float gameWidth)
 {
     jumped = true;
+    std::cout<<direction;
+    moveElement(direction, moveSpeed);
+    /*
     if(direction == 'u')
     {
         if(!(getPosition().y-moveSpeed< 0.f + getHeight())) //if tries to exceed boundaries, don't move (avoids misalignment with icebergs)
         {
-            fSprite.move(0, -moveSpeed);
+            move(, -moveSpeed);
         }
     }
     else if(direction == 'd')
     {
-        fSprite.move(0, moveSpeed);
+        move(0, moveSpeed);
         //screen colision on the bottom
         if(getPosition().y>gameHeight) //check to base line
-            setPostion(getPosition().x,gameHeight);
+            setPosition(getPosition().x,gameHeight);
     }
+    */
 }
 //returns if frostbite has jumped
 const bool Frostbite::hasJumped() const
 {
     return jumped;
 }
+/*
 //get and set players positon (should be center of player for now)
 const sf::Vector2f Frostbite::getPosition() const
 {
     return fSprite.getPosition();
 }
-void Frostbite::setPostion(float x, float y)
-{
-    fSprite.setPosition(x,y);
-}
+*/
+
 
 //get players width
 const float Frostbite::getWidth() const
 {
-    return fSprite.getTexture()->getSize().x * fSprite.getScale().x;
+    return getSize().x ;
 }
 const float Frostbite::getHeight() const
 {
-    return fSprite.getTexture()->getSize().y * fSprite.getScale().y;
+    return getSize().y ;
 }
 
+/*
 //get bounding box
 sf::FloatRect Frostbite::getBounding () const
 {
     return fSprite.getGlobalBounds();
 }
+*/
 //resets characterisitcs
 void Frostbite::reset()
 {
     jumped = false;
+}
+Frostbite::~Frostbite()
+{
 }
--- game-source-code/frostbite.h
+++ game-source-code/frostbite.h
@@ -4,15 +4,17 @@
 #include <iostream>
 #include <SFML\Graphics.hpp>
 #include "motion.h"
+#include "element.h"
 
-class Frostbite: public Motion
+class Frostbite: public Motion, public Element
 {
     public:
-        Frostbite(){
+        Frostbite(): Element("resources/frostbite.png",sf::Vector2f(1.f,1.f)){
         //default
-        }
+
+        };
         Frostbite(std::string imDirectory, const sf::Vector2f &Size);
-        void draw(sf::RenderWindow &window);
+        //void draw(sf::RenderWindow &window);
 
         //move player around
         virtual void move(char direction, const sf::RenderWindow &window, float moveSpeed) override;
@@ -21,13 +23,13 @@ class Frostbite: public Motion
         //returns if frostbite has jumped
         const bool hasJumped() const;
         //get and set players positon (should be center of player for now);
-        const sf::Vector2f getPosition() const ;
-        void setPostion(float x, float y);
+        //const sf::Vector2f getPosition() const ;
+       // void setPostion(float x, float y);
         //get players width
         const float getWidth() const ;
         const float getHeight() const ;
         //return global boundaries
-        sf::FloatRect getBounding () const;
+        //sf::FloatRect getBounding () const;
         //resets Frostbite characteristics
         void reset();
         virtual ~Frostbite();
@@ -35,8 +37,8 @@ class Frostbite: public Motion
     protected:
 
     private:
-        sf::Texture fTexture;
-        sf::Sprite fSprite;
+       // sf::Texture fTexture;
+       // sf::Sprite fSprite;
         bool jumped; //to see if the player has jumped, has implicatiins when counting icebergs for score
 };
 
