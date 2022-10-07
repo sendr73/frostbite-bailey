8aeeaaa0705680edc79ecb88bbe3b9e27c9c99af
Koshti01 <ruthannwright01@gmail.com>
2022-10-07 19:09:56 +0200

    Element in Icebege Part 2

--- game-source-code/iceberg.h
+++ game-source-code/iceberg.h
@@ -4,21 +4,22 @@
 #include <iostream>
 #include <SFML\Graphics.hpp>
 #include "motion.h"
+#include "element.h"
 
-class Iceberg: public Motion
+class Iceberg: public Motion, public Element
 {
     public:
-        Iceberg(){
+        Iceberg(): Element("resources/iceberg.png",sf::Vector2f(1.f,1.f)),direction{'r'},landed{false}{
         //default constructor
         }
         Iceberg(std::string imDirectory, const sf::Vector2f &Size);
         //draws the iceberg on the window
-        void draw(sf::RenderWindow &window);
+       // void draw(sf::RenderWindow &window);
         //moves the iceberg
         virtual void move(char direction, const sf::RenderWindow &window, float moveSpeed) override;
         //getter and setter for the position of the iceberg
-        const sf::Vector2f getPosition() const;
-        void setPosition(float x, float y);
+       // const sf::Vector2f getPosition() const;
+        //void setPosition(float x, float y);
         //getter and setter for the direction of the iceberg
         const char getDirection() const;
         void setDirection(const char dir);
