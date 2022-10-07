542522d757bdf41830b2c04785594bb802434110
sendr7 <d.sender07@gmail.com>
2022-10-07 11:14:17 +0200

    Clarifying Splash Screen

--- game-source-code/splashscreen.cpp
+++ game-source-code/splashscreen.cpp
@@ -20,12 +20,11 @@ splashScreen::splashScreen(const float gameWidth, const float gameHeight)
 
 
     paragraph.setString("Use the LEFT and RIGHT arrow keys to move Frostbite left and right \n"
-                        "and the UP and DOWN arrow keys to jump platforms, up and down.  \n"
-                        "Try to land on the moving icebergs to build the igloo. \n"
-                        "Beware of the crabs and clams and do not fall in the water\n"
-                        "as you will loose a life. \n"
-                        "If the temperature decreases below zero you will freeze. \n"
-                        "To win build and enter the igloo before your lives run out. \n \n"
+                        "and the UP and DOWN arrow keys to jump platforms, up and down. \n"
+                        "Try to land on the moving icebergs to build the igloo. Beware of the\n"
+                        "crabs and clams and do not fall in the water, as you will loose a \n"
+                        "life. If the temperature decreases below zero you will freeze and \n"
+                        "lose a life. To win build and enter the igloo before your lives run out\n\n"
                         "Press the ENTER key to start.");
     paragraph.setFont(font); //set font, color etc
     paragraph.setCharacterSize(30);
