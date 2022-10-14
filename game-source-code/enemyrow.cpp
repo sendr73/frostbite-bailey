0028e82beb8cc2dc2bb3d39bc68bd5d49446a019
Ruth-Ann Wright <ruthannwright01@gmail.com>
2022-10-14 23:49:34 +0200

    Creating Enemy Row Class

--- game-source-code/enemyrow.cpp
+++ game-source-code/enemyrow.cpp
@@ -1,4 +1,5 @@
 #include "enemyrow.h"
+#include <iostream>
 
 EnemyRow::EnemyRow()
 {
@@ -7,27 +8,43 @@ EnemyRow::EnemyRow()
 
 EnemyRow::EnemyRow(std::string imDirectory, const MovementType &movement, float screenWidth, float screenHeight)
 {
+    std::cout<<"Constructor";
 
-    Enemy enemy_ = Enemy(imDirectory);
-    enemy_row=vector<Enemy>(4,enemy_); //initializes 1D
+    enemy_row=vector<Enemy>(4,enemy_crab); //initializes 1D
     //is setting position presentation or design
     for(int j = 0; j<enemy_row.size(); j++) //loops through columns
     {
-        enemy_row[j].setPosition((20)+2*(j)*enemy_.getWidth()
-                               ,(0.3*screenHeight)); //.y)+(frostbite.getPosition().y)+10);
+        enemy_row[j].setPosition((40)+2*(j)*enemy_crab.getWidth()
+                                 ,(0.55*screenHeight+100)); //.y)+(frostbite.getPosition().y)+10);
         enemy_row[j].setDirection('l');
     }
+    movement_type_ = movement;
 
 }
 void EnemyRow::move(char direction, const sf::RenderWindow &window, float deltaTime)
 {
+
+
+    auto gameWidth = window.getSize().x;
+    auto gameHeight = window.getSize().y;
+
     for(int i = 0; i<enemy_row.size(); i++) //size of crab row == size of clam row
     {
-        enemy_row[i].move(direction,window, enemySpeed*deltaTime);
-        //enemy_row[i].move(,'r',window, enemySpeed*deltaTime);
+        enemy_row[i].move('l',window, deltaTime);
+        //moveSprite(clamrow[i],'r',window, enemySpeed*deltaTime);
     }
 
 }
+void EnemyRow::draw(sf::RenderWindow &window)
+{
+    //std::cout<<"Draw";
+    for(auto i =0; i<enemy_row.size(); i++)
+    {
+        //std::cout<<"Draw 2";
+        enemy_row[i].draw(window);
+    }
+}
+
 
 EnemyRow::~EnemyRow()
 {
--- game-source-code/enemyrow.h
+++ game-source-code/enemyrow.h
@@ -5,6 +5,8 @@
 //#include <iostream>
 #include "enemy.h"
 #include "motion.h"
+#include "print.h"
+
 using namespace std;
 //should add  enum for movement type
 enum class MovementType{Glide = 1, Stagger = 2};
@@ -14,14 +16,17 @@ class EnemyRow: public Motion
         EnemyRow();
         EnemyRow(std::string imDirectory, const MovementType &movement,float screenWidth, float screenHeight );
         virtual void move(char direction, const sf::RenderWindow &window, float moveSpeed) override;
+        void draw(sf::RenderWindow &window);
         virtual ~EnemyRow();
 
     protected:
 
     private:
-        const float enemySpeed = 150.0f;
+        Enemy enemy_crab = Enemy("resources/crab.png"); //enemy (set to crab)
+        Enemy enemy_clam = Enemy("resources/clam.png"); //enemy (set to crab)
         const char direction = 'l'; //should be able to be changed
         vector<Enemy> enemy_row;
+        Enemy overlap_enemy = Enemy("resources/frostbite.png");
         MovementType movement_type_ = MovementType::Glide; //initialized as glide
 };
 
