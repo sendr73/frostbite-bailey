9bccc0cde5c5ff960c2cfd88a20803824f2a8079
Ruth-Ann Wright <ruthannwright01@gmail.com>
2022-10-15 13:09:57 +0200

    Neating up Enemies Part 1

--- game-source-code/EnemyMatrix.cpp
+++ game-source-code/EnemyMatrix.cpp
@@ -1,45 +1,36 @@
 #include "EnemyMatrix.h"
 
-EnemyMatrix::EnemyMatrix()
+EnemyMatrix::EnemyMatrix() //populate the enemy matrix array by pushing back the two enemy rows
 {
-
     enemy_matrix.push_back(enemy_row_crab);
-
     enemy_matrix.push_back(enemy_row_clam);
 }
-void EnemyMatrix::move(char direction, const sf::RenderWindow &window, float moveSpeed)
+void EnemyMatrix::move(char direction, const sf::RenderWindow &window, float moveSpeed) //move both rows
 {
-
-    enemy_matrix[0].move('l', window, moveSpeed);
-    enemy_matrix[01].move('r', window, moveSpeed);
-
+    enemy_matrix[0].move('q', window, moveSpeed); //q is passed in as a direction as direction is specified in the rows themselves
+    enemy_matrix[01].move('q', window, moveSpeed);
 }
 void EnemyMatrix::collision(Frostbite &frostbite,const sf::RenderWindow &window)
 {
     sf::FloatRect frostbite_boundary_box = frostbite.getBoundaries();
-    for(auto i =0; i<enemy_matrix.size(); i++)
+    for(auto enemy_row_it: enemy_matrix)
     {
-        auto enemy_rowBoundary = enemy_matrix[i].getRowBoundaries();
-        for(auto i =0; i<enemy_rowBoundary.size(); i++)
+        auto enemy_row_boundary = enemy_row_it.getRowBoundaries(); //get vector of the bourdary boxes of each enemy
+        for(auto enemy_boundary_it : enemy_row_boundary)
         {
-          if(frostbite_boundary_box.intersects(enemy_rowBoundary[i]))
+          if(frostbite_boundary_box.intersects(enemy_boundary_it)) //if they intersect, the enemy will push frostbite along with them, generally causing him to fall off an iceberg
             {
-                std::cout<<"Intersect";
-                frostbite.move(enemy_matrix[i].getDirection(),window, enemy_matrix[i].getSpeed());
+                frostbite.move(enemy_row_it.getDirection(),window, enemy_row_it.getSpeed());
             }
         }
     }
 }
-void EnemyMatrix::draw(sf::RenderWindow &window)
+void EnemyMatrix::draw(sf::RenderWindow &window) //should be separated into a different class (print class)
 {
-
-    //std::cout<<"Draw";
     for(auto i =0; i<enemy_matrix.size(); i++)
     {
-        //std::cout<<"Draw 2";
         enemy_matrix[i].draw(window);
     }
-
 }
 
 EnemyMatrix::~EnemyMatrix()
--- game-source-code/EnemyMatrix.h
+++ game-source-code/EnemyMatrix.h
@@ -1,4 +1,6 @@
 //Class consisting of Enemy rows through composition
+//This is the enemy class called by the game
+//It uses class motion to move and deals with collisions through collision class
 #ifndef ENEMYMATRIX_H
 #define ENEMYMATRIX_H
 #include <vector>
@@ -11,7 +13,7 @@ using namespace std;
 class EnemyMatrix: public Motion, public Collisions
 {
     public:
-        EnemyMatrix();
+        EnemyMatrix(); //default construction is fine, not parameters are required to be passed through
         virtual void move(char direction, const sf::RenderWindow &window, float moveSpeed) override;
         virtual void collision(Frostbite &frostbite, const sf::RenderWindow &window) override;
         void draw(sf::RenderWindow &window);
@@ -20,10 +22,10 @@ class EnemyMatrix: public Motion, public Collisions
     protected:
 
     private:
-        EnemyRow enemy_row_crab = EnemyRow("resources/crab.png", MovementType::Glide, 40.f, 460.f);
-        EnemyRow enemy_row_clam = EnemyRow("resources/clam.png", MovementType::Glide, 40.f, 560.f);
+        //should implement smart pointers
+        EnemyRow enemy_row_crab = EnemyRow("resources/crab.png", MovementType::Glide, 40.f, 460.f, 'l'); //two enemy rows created with different textures
+        EnemyRow enemy_row_clam = EnemyRow("resources/clam.png", MovementType::Glide, 40.f, 565.f, 'r'); //they are pushed back into the enemy-matrix at construction
         vector<EnemyRow> enemy_matrix;
-
 };
 
 #endif // ENEMYMATRIX_H
