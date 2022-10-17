285d822864a95cf00bc0b9c83614c3147cde0ac0
Ruth-Ann Wright <ruthannwright01@gmail.com>
2022-10-17 19:30:59 +0200

    Minor Changes

--- game-source-code/enemy.cpp
+++ game-source-code/enemy.cpp
@@ -1,22 +1,14 @@
 #include "enemy.h"
 
-Enemy::Enemy(): Element("resources/crab.png",sf::Vector2f(1.f,1.f))
+Enemy::Enemy(): Element("resources/crab.png",sf::Vector2f(1.f,1.f)), enemySpeed(70.0f), direction('r')
 {
-    //ctor
 }
 
-Enemy::Enemy(std::string imDirectory): Element(imDirectory,sf::Vector2f(1.f,1.f))
+Enemy::Enemy(std::string imDirectory): Element(imDirectory,sf::Vector2f(1.f,1.f)), enemySpeed(70.0f), direction('r')
 {
-
 }
-/*
-void Enemies::draw(sf::RenderWindow &window)
-{
-    window.draw(eSprite);
-}
-*/
 
-void Enemy::move(char direction, const float &x, const float &y, float deltaTime) //enemeis must store its own speed
+void Enemy::move(char direction, const float &x, const float &y, float deltaTime)
 {
     moveElement(direction, deltaTime*enemySpeed);
     if(getPosition().x+getWidth()/2<=0)
@@ -29,17 +21,6 @@ void Enemy::move(char direction, const float &x, const float &y, float deltaTime
         setPosition(getWidth()/2,getPosition().y);
     }
 }
-/*
-const sf::Vector2f Enemies::getPosition() const
-{
-    return eSprite.getPosition();
-}
-
-void Enemies::setPosition(float x, float y)
-{
-    eSprite.setPosition(x,y);
-}
-*/
 
 const char Enemy::getDirection() const
 {
@@ -63,14 +44,6 @@ const float Enemy::getHeight() const
 {
     return getSize().y;
 }
-
-/*
-//get bounding box
-sf::FloatRect Enemies::getBounding () const
-{
-    return eSprite.getGlobalBounds();
-}
-*/
 
 Enemy::~Enemy()
 {
