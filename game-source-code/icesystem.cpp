3c76833bc4ed08dd045d15c846ef14b71d4b3db8
sendr7 <d.sender07@gmail.com>
2022-10-19 00:17:01 +0200

    Commenting and formatting icesystem.cpp

--- game-source-code/icesystem.cpp
+++ game-source-code/icesystem.cpp
@@ -1,5 +1,5 @@
 #include "icesystem.h"
-
+// constructor for IceSystem, hardcoding is more efficient since there will always be 4 rows
 IceSystem::IceSystem()
 {
     icesystem_.push_back(i1);
@@ -7,7 +7,7 @@ IceSystem::IceSystem()
     icesystem_.push_back(i3);
     icesystem_.push_back(i4);
 }
-
+// moves each Icerow in the IceSystem using Icerow's move function
 void IceSystem::move(char direction, const float &x, const float &y, float moveSpeed) //move both rows
 {
     for(int i = 0; i < row; i++)
@@ -15,43 +15,45 @@ void IceSystem::move(char direction, const float &x, const float &y, float moveS
         icesystem_[i].move('q', x,y, moveSpeed); //q is passed in as a direction as direction is specified in the rows themselves
     }
 }
-
+// checks the collision for each row and returns the index of the collision. -1 if no collision
 int IceSystem::collision(Frostbite &frostbite, const float &x, const float &y, const float &deltaTime)
 {
-    int index = -1;
-    int num = 0;
+    int index = -1; // default index
+    int num = 0; // int num variable (easier than converting an iterator into an int)
     sf::FloatRect frostbite_boundary_box = frostbite.getBoundaries();
     for(auto ice_row_it:icesystem_)
     {
-        num++;
-        auto ice_row_boundary = ice_row_it.getRowBoundaries(); //get vector of the bourdary boxes of each enemy
+        num++; // increment num, tracking the row number
+        auto ice_row_boundary = ice_row_it.getRowBoundaries(); // get vector of the bourdary boxes of each enemy
         for(auto ice_boundary_it : ice_row_boundary)
         {
-          if(frostbite_boundary_box.intersects(ice_boundary_it)) //if they intersect, the enemy will push frostbite along with them, generally causing him to fall off an iceberg
+          if(frostbite_boundary_box.intersects(ice_boundary_it))
             {
+                // frostbite moves with the iceberg. Can use index of 0 since the speed is consistant
                 frostbite.move(ice_row_it[0].getDirection(),x,y,ice_row_it[0].getSpeed()*deltaTime);
-                index = num;
+                index = num; // index stores the row number
+                break;
             }
         }
     }
     return index;
 }
-
+// operator overload that returns the Icerow at index
 Icerow IceSystem::operator[](const int& index)
 {
     return icesystem_[index];
 }
-
+// returns the size of the IceSystem (should always be 4)
 const int IceSystem::size() const
 {
     return icesystem_.size();
 }
-
+// executes the landedOn function for the row at index, i
 void IceSystem::rowLandedOn(const int &i)
 {
     icesystem_[i].landedOn();
 }
-
+// goes through all rows and executes the draw function in Icerow
 void IceSystem::draw(sf::RenderWindow &window) //should be separated into a different class (print class)
 {
     for(auto i =0; i<icesystem_.size(); i++)
@@ -59,12 +61,12 @@ void IceSystem::draw(sf::RenderWindow &window) //should be separated into a diff
         icesystem_[i].draw(window);
     }
 }
-
+// executes reverse for the i-th Icerow in IceSystem
 void IceSystem::reverse(const int &i)
 {
     icesystem_[i].reverse();
 }
-
+// resets all Icebergs in IceSystem
 void IceSystem::reset()
 {
     for(int i = 0; i < icesystem_.size();i++)
@@ -72,7 +74,7 @@ void IceSystem::reset()
         icesystem_[i].reset();
     }
 }
-
+// default destuctor
 IceSystem::~IceSystem()
 {
     //dtor
