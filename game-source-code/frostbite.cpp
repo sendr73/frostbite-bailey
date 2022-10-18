07cbd38dcbc6bf878f8e2248c03fba6d7a3965f2
sendr7 <d.sender07@gmail.com>
2022-10-18 20:02:40 +0200

    Commenting and formatting frostbite.cpp

--- game-source-code/frostbite.cpp
+++ game-source-code/frostbite.cpp
@@ -5,13 +5,7 @@ Frostbite::Frostbite(std::string imDirectory, const sf::Vector2f &Size): Element
     setPosition(200,100); //set Sprite to top left
     jumped = false;
 }
-/*void Frostbite::draw(sf::RenderWindow &window)
-{
-    window.draw(fSprite);
-}
-*/
-//move player around
-
+// move player around horizontally
 void Frostbite::move(char direction, const float &x, const float &y, float moveSpeed)
 {
     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) //set direction from input keyboard
@@ -23,64 +17,59 @@ void Frostbite::move(char direction, const float &x, const float &y, float moveS
         direction = 'r';
     }
     moveElement(direction,moveSpeed);
-
-    //screen colision on the left
-    if(getPosition().x<0 + getWidth()/2)
+    // screen collision logic on the left
+    if(getPosition().x<0 + getWidth()/2) //adjust the sides by width/2 when checking left and right
     {
-        //adjust the sides by width/2 when checking left and right
         setPosition(getWidth()/2,getPosition().y);
     }
-    //screen colision on the right
+    // screen collision logic on the right
     if(getPosition().x>x-getWidth()/2) //adjust the sides by width/2 when checking left and right
     {
         setPosition(x-getWidth()/2, getPosition().y);
     }
 
 }
-//make player jump
+// make player jump vertically (was added with the intention to animate)
 void Frostbite::jump(char direction, float moveSpeed, const float gameHeight, const float gameWidth)
 {
     jumped = true;
     moveElement(direction, moveSpeed);
-
-    if(getPosition().y<0.375*gameHeight) //collision at top of playing screen (below score etc)
+    if(getPosition().y<0.375*gameHeight) // collision at upper limit of playing screen (below score etc)
     {
-        setPosition(getPosition().x,0.375*gameHeight); //can one enter the igloo?
+        setPosition(getPosition().x,0.375*gameHeight); // limits at the upper bound, ensures no height desync w/ icebergs
     }
 
-    if(getPosition().y+getHeight()>gameHeight) //collision at bottom of screen
+    if(getPosition().y+getHeight()>gameHeight) // collision at bottom limit of playing screen
     {
-        setPosition(getPosition().x,gameHeight);
+        setPosition(getPosition().x,gameHeight); // limits to the bottom of the screen, respawns - desync irrelevent
     }
-
-
 }
-//returns if frostbite has jumped
+// returns if frostbite has jumped
 const bool Frostbite::hasJumped() const
 {
     return jumped;
 }
-
-//get players width
+// get players width
 const float Frostbite::getWidth() const
 {
     return getSize().x ;
 }
+// get players height
 const float Frostbite::getHeight() const
 {
     return getSize().y ;
 }
-
+// gets player speed
 const float Frostbite::getSpeed() const
 {
     return speed;
 }
-
-//resets characterisitcs
+// resets characteristics
 void Frostbite::reset()
 {
     jumped = false;
 }
+// virtual destructor
 Frostbite::~Frostbite()
 {
 }
