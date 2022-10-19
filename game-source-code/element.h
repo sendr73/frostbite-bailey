2fa98489392785f132a0c9e922e032804a8a639e
sendr7 <d.sender07@gmail.com>
2022-10-19 02:16:46 +0200

    Commenting and formatting element.h

--- game-source-code/element.h
+++ game-source-code/element.h
@@ -1,4 +1,10 @@
 //basic element class, used later for implementation (probably should be changed to be used in compositon)
+/**
+ * \brief Element Class
+ * Base class with functions that all Sprites will inherit
+ * @author Ruth-Ann Wright (2351852)
+ * @author Daron Sender (2332451)
+ */
 #ifndef ELEMENT_H
 #define ELEMENT_H
 #include <string>
@@ -8,22 +14,79 @@
 class Element
 {
     public:
+        /**
+        * \brief Default Element constructor
+        */
         Element();
+
+        /**
+        * \brief Parametized Element constructor
+        * A texture is taken in as a string imDirectory, and scale is taken in. This is then assigned to texture_ member.
+        * and then used to create a Sprite, sprite_.
+        * \param imDirectory is the directory of the texture
+        * \param Size is the scale of the object created.
+        */
         Element(std::string imDirectory, const sf::Vector2f &Size);
-        const sf::Vector2f getPosition() const ;
-        void setPosition(float x, float y);         //set position
+
+        /**
+        * \brief Gets the x and y coordinate of Element
+        * \return The position Vector2f containing the x and y coordinate
+        */
+        const sf::Vector2f getPosition() const;
+
+        /**
+        * \brief Sets the x and y coordinate of Element
+        * \param The x coordinate
+        * \param The y coordinate
+        */
+        void setPosition(float x, float y);
+
+        /**
+        * \brief Gets the size of the Element
+        * Since a Sprite has no size, the scale of sprite_ and size of the texture were multiplied
+        * to return a unsigned int representative of the size of the Element.
+        * \return A Vector2f containing the width and height of sprite_
+        */
         const sf::Vector2<unsigned int> getSize() const;
+
+        /**
+        * \brief Gets the Boundaries of Element
+        * These boundaries are used to check that objects are colliding. it is not const because some
+        * assymetrical textures need to be compensated for (Iceberg)
+        * \return A FloatRect containing boundaries of sprite_
+        */
         sf::FloatRect getBoundaries() const;
+
+        /**
+        * \brief Moves the Element
+        * \param The direction that the Element is required to move
+        * \param The speed at which the Element is moving
+        */
         void moveElement(char direction, float moveSpeed);
+
+        /**
+        * \brief Sets the texture of the Element
+        * It is required to be public in case the Element needs to chnage appearance
+        * \param imDirectory is the directory of the texture
+        */
         void setTexture(std::string imDirectory);
-        void draw(sf::RenderWindow &window); //overriding the draw function from print class, interface inheritance
+
+        /**
+        * \brief Draws the Sprite on a window
+        * \param Window on which Element is drawn
+        */
+        void draw(sf::RenderWindow &window);
+
+        /**
+        * \brief Default IceSystem Destructor
+        */
         virtual ~Element();
-        sf::Sprite sprite_;
-        sf::Texture texture_;
+
     protected:
 
     private:
-
+        sf::Sprite sprite_;
+        sf::Texture texture_;
 };
 
 #endif // ELEMENT_H
